c47bcc928652fe698345dee769090280d5c7e075
Med Diyae GHBALOU <meddiyae.ghbalou@gmail.com>
2025-05-19 16:24:51 +0100

    delete product off price

--- proxy-servicenow/api/productOfferingPrice/index.js
+++ proxy-servicenow/api/productOfferingPrice/index.js
@@ -5,11 +5,11 @@ const authMiddleware = require('../../middleware/auth');
 // Import controllers
 const getAll = require('../../controllers/ProductOfferingPrice/getAllProductOfferingPrice');
 const create = require('../../controllers/ProductOfferingPrice/createProductOfferingPrice');
-const update = require('../../controllers/ProductOfferingPrice/updateProductOfferingPrice');
+const deleteProd = require('../../controllers/ProductOfferingPrice/deleteProductOfferingPrice');
 
 // Define routes
 router.get('/product-offering-price', getAll);
 router.post('/product-offering-price', create);
-router.patch('/product-offering-price/:id', update);
+router.delete('/product-offering-price/:id', deleteProd);
 
 module.exports = router;
--- proxy-servicenow/controllers/ProductOfferingPrice/deleteProductOfferingPrice.js
+++ proxy-servicenow/controllers/ProductOfferingPrice/deleteProductOfferingPrice.js
@@ -1,20 +1,21 @@
 const axios = require('axios');
 const snConnection = require('../../utils/servicenowConnection');
 const handleMongoError = require('../../utils/handleMongoError');
+const productOfferingPrice = require('../../models/productOfferingPrice');
 
 
 module.exports = async (req, res) => {
   try {
     // Mettre à jour dans ServiceNow
     const connection = snConnection.getConnection(req.user.sn_access_token);
-    const snResponse = await axios.patch(
-      `${connection.baseURL}api/sn_tmf_api/catalogmanagement/productOfferingPrice/${req.params.id}`,
+    const snResponse = await axios.delete(
+      `${connection.baseURL}/api/sn_tmf_api/catalogmanagement/productOfferingPrice/${req.params.id}`,
       req.body,
       { headers: connection.headers }
     );    
     // Mettre à jour dans MongoDB
     try {
-      await ProductOfferingPrice.findOneAndUpdate(
+      await productOfferingPrice.findOneAndDelete(
         { sys_id: req.params.id },
         req.body,
         { new: true }
@@ -25,7 +26,7 @@ module.exports = async (req, res) => {
     
     res.json(snResponse.data);
   } catch (error) {
-    console.error('Error updating product offering price :', error);
+    console.error('Error deleting product offering price :', error);
     const status = error.response?.status || 500;
     const message = error.response?.data?.error?.message || error.message;
     res.status(status).json({ error: message });
