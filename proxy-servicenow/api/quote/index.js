7138827c1b3ef58c9440cecd45ecf9ed6229305a
Yassir Haiddouch <haiddouchy5@gmail.com>
2025-05-20 11:03:57 +0100

    fix quote conflits

--- proxy-servicenow/api/quote/index.js
+++ proxy-servicenow/api/quote/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // Import controllers
- const getAll = require('../../controllers/quote/getall')
-// const create = require('../../controllers/quota/create')
+const getAll = require('../../controllers/quote/getall')
 const update = require('../../controllers/quote/update')
+const create = require('../../controllers/quote/create')
+const deleteQuote = require('../../controllers/quote/delete')
 
 router.get('/quote', getAll);
-// router.post('/quota', create);
+router.post('/quote', create);
+router.delete('/quote/:sysId', deleteQuote);
 router.patch('/quote', update);
 
 module.exports = router;
+
