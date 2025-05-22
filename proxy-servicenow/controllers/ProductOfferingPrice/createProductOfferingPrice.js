3cd779f4b46179c87e9f1e96c0ffe979c9b3edd4
lilassaf <149604598+lilassaf@users.noreply.github.com>
2025-05-22 17:34:11 +0100

    createProductOfferingPrice mongoDB

--- proxy-servicenow/controllers/ProductOfferingPrice/createProductOfferingPrice.js
+++ proxy-servicenow/controllers/ProductOfferingPrice/createProductOfferingPrice.js
@@ -2,20 +2,57 @@ const axios = require('axios');
 const ProductOfferingPrice = require('../../models/productOfferingPrice');
 const snConnection = require('../../utils/servicenowConnection');
 const handleMongoError = require('../../utils/handleMongoError');
+const ProductOffering = require ('../../models/ProductOffering');
+const PriceList = require ('../../models/priceList');
 
 async function createProductOfferingPrice(req, res = null) {
+  
+  
   try {
-    // 1. Create in ServiceNow
+    let productOffering;
+    let priceList;
+    
+    try {
+      productOffering = await ProductOffering.findById(req.body.productOffering.id);
+      priceList = await PriceList.findById(req.body.priceList.id)
+      let payload = {
+      ...req.body,
+      priceList:{
+        id: priceList.sys_id
+      },
+      productOffering:{
+        id: productOffering.id
+      }
+      
+    }
+    //1. Create in ServiceNow
     const connection = snConnection.getConnection(req.user.sn_access_token);
     const snResponse = await axios.post(
       `${connection.baseURL}/api/sn_tmf_api/catalogmanagement/productOfferingPrice`,
-      req.body,
+      payload,
       { headers: connection.headers }
     );
 
     // 2. Prepare MongoDB document from ServiceNow response
     const snData = snResponse.data;
-    const mongoPrice = new ProductOfferingPrice({sys_id: snData.id, ...snData});
+    const mongoPayload = {
+        "sys_id":snData.id,
+        "name": snData.name,
+        "price": snData.price,
+        "lifecycleStatus": snData.lifecycleStatus,
+        "validFor": snData.validFor,
+        "productOffering": productOffering._id ,
+        "priceType": snData.priceType,
+        "recurringChargePeriodType": snData.recurringChargePeriodType,
+        "unitOfMeasure": snData.unitOfMeasure,
+        "priceList": priceList._id,
+        "@type": "ProductOfferingPrice",
+        "id": snData.id,
+        "state": snData.state,
+        "href": snData.href
+    }
+    //console.log(JSON.stringify(mongoPayload,null, 2));
+    const mongoPrice = new ProductOfferingPrice(mongoPayload);
 
     // 3. Save to MongoDB
     try {
@@ -24,14 +61,25 @@ async function createProductOfferingPrice(req, res = null) {
       if(res){
         // 4. Return success response
         res.status(201).json({
-          serviceNow: snData,
-          mongoDB: mongoPrice.toObject()
+          ...snData,
+          _id: mongoPrice._id.toString(), // Include MongoDB ID in the response
+          mongoId: mongoPrice._id.toString() // Alternative field name if preferred
         });
       }
       
     } catch (mongoError) {
       return handleMongoError(res, snData, mongoError, 'creation');
     }
+      
+    } catch (mongoError) {
+      return handleMongoError(res, snData, mongoError, 'creation');
+    }
+    
+    
+    
+      
+    
+    
 
   } catch (error) {
     console.error('Error creating product offering price:', error);
