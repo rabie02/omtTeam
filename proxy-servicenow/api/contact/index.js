fd920995590229254bb577445fa8533959bb9348
Ismail Bouzarhoun <91149489+Ismailbouzarhoun@users.noreply.github.com>
2025-06-11 17:55:55 +0100

    get all account with contacts and locations

--- proxy-servicenow/api/contact/index.js
+++ proxy-servicenow/api/contact/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 // Import controllers
 const create = require('../../controllers/contact/create');
+const getByAccount = require('../../controllers/contact/getByAccount');
 
 //routes
 router.post('/contact', create)
+router.get('/contact/account/:id', getByAccount)
 
 module.exports = router;
--- proxy-servicenow/models/account.js
+++ proxy-servicenow/models/account.js
@@ -1,22 +1,34 @@
 const mongoose = require('mongoose');
 
 const accountSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    sys_id: String,
-    sys_created_on: String,
-    sys_created_by: String,
-    business_owners: String,
-    phone: String,
-    industry: String,
-    email: String,
-    website: String,
-    customer: String
+  name: {
+    type: String,
+    required: true,
+  },
+  sys_id: String,
+  sys_created_on: {
+    type: Date // Changed from String to Date (optional)
+  },
+  sys_created_by: String,
+  business_owners: String,
+  phone: String,
+  industry: String,
+  email: String,
+  website: String,
+  customer: String,
+
+  contacts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Contact'
+  }],
+
+  locations: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Location'
+  }]
 }, {
-    timestamps: true, // Automatically adds createdAt and updatedAt
-    strict: false
+  timestamps: true,
+  strict: false
 });
 
 module.exports = mongoose.model('Account', accountSchema, 'accounts');
