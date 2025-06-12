a84a5f6b65d44677e1668ddc89be2648fd053437
MCDIYAE <meddiyae.ghbalou@gmail.com>
2025-06-12 12:14:39 +0100

    delete / get all : location & contact

--- proxy-servicenow/api/contact/index.js
+++ proxy-servicenow/api/contact/index.js
@@ -4,9 +4,16 @@ const router = express.Router();
 // Import controllers
 const create = require('../../controllers/contact/create');
 const getByAccount = require('../../controllers/contact/getByAccount');
+const getContact = require('../../controllers/contact/getall');
+const delContact = require('../../controllers/contact/delete');
+
 
 //routes
 router.post('/contact', create)
-router.get('/contact/account/:id', getByAccount)
+router.get('/contact/account/:id', getByAccount);
+router.get('/contact/', getContact)
+router.delete('/contact/:id', delContact)
+
+
 
 module.exports = router;
--- proxy-servicenow/api/location/index.js
+++ proxy-servicenow/api/location/index.js
@@ -3,10 +3,18 @@ const router = express.Router();
 
 // Import controllers
 const create = require('../../controllers/location/create');
+const getall = require('../../controllers/location/getall');
+const delLocation = require('../../controllers/location/delete');
 const getAdresse = require('../../controllers/location/getAdresse');
 
 //routes
 router.post('/location', create);
 router.get('/reverse-geocode', getAdresse);
+router.get('/location', getall);
+router.get('/location/:id', delLocation);
+
+
+
+
 
 module.exports = router;
